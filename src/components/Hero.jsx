078aa1b5427ba1b998b2c9ac30271b0fcc88a24e
pkihiu <p.kihiu@alustudent.com>
2024-04-05 11:45:22 +0200

    update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,11 +17,11 @@ const Hero = () => {
         <h1 className='text-black text-3xl font-bold uppercase'>Looking for a place to relax?</h1>
         <h2 className='py-4 font-semibold'>Find your dream getaway today.</h2>
         <form
-          className='max-w-[700px] mx-auto w-full border p-1 rounded-md text-black bg-gray-100/90'
+          className='max-w-[700px] mx-auto w-full sm:px-6 border p-1 rounded-md text-black bg-gray-100/90'
         >
           <div className='flex items-center'>
             <input
-              className='bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none flex-1'
+              className='bg-transparent w-full sm:w-[300px] md:w-[400px] font-[Poppins] focus:outline-none flex-1'
               type='text'
               placeholder='Search Destinations'
             />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,24 @@ import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center px-4 h-20 absolute z-10 w-full'>
+    <div className='flex flex-col md:flex-row justify-between items-center px-4 h-20 absolute z-10 w-full'>
       <h1 className='text-3xl font-bold uppercase p-5 pl-10'>Jadoo.</h1>
-      <ul className='flex items-center flex-grow justify-center'>
+      <ul className='flex flex-col md:flex-row items-center'>
         <li className='pr-10 text-white font-semibold'>Home</li>
-        <li className='pr-10  text-white font-semibold'>Destinations</li>
-        <li className='pr-10  text-white font-semibold'>Travel</li>
-        <li className='pr-10  text-white font-semibold'>View</li>
-        <li className='pr-10  text-white font-semibold'>Book</li>
+        <li className='pr-10 text-white font-semibold'>Destinations</li>
+        <li className='pr-10 text-white font-semibold'>Travel</li>
+        <li className='pr-10 text-white font-semibold'>View</li>
+        <li className='pr-10 text-white font-semibold'>Book</li>
       </ul>
       <div className='flex items-center'>
-        <FontAwesomeIcon icon={faSearch} className='text-white mr-8' /> 
-        <FontAwesomeIcon icon={faUser} className='text-white mr-8' /> 
+        <FontAwesomeIcon icon={faSearch} className='text-white mr-4 md:mr-8' /> 
+        <FontAwesomeIcon icon={faUser} className='text-white mr-4 md:mr-8' /> 
       </div>
     </div>
   );
 };
 
 export default Navbar;
+
 
 
