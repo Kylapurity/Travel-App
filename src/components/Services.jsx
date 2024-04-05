5679d2ae2eead2cf60493412ba9e712841061a7f
pkihiu <p.kihiu@alustudent.com>
2024-04-05 12:19:27 +0200

    update Hero

---
+++ src/components/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import beach from '../assets/beach.jpg';
+import flights from '../assets/flights.jpg';
+import Hot_Air from '../assets/Hot-Air-Balloon.jpeg';
+import tarvel from '../assets/travel-concept-with-baggage.jpg';
+
+const Services = () => {
+  return (
+    <div className='flex justify-center items-center pt-5'>
+      <h1 className='text-3xl font-bold underline uppercase'>We Offer Best Services</h1>
+
+    </div>
+  )
+}
+
+export default Services
+
