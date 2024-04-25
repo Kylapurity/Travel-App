c6f6ad95e7d9f9853c9b4065eea8eb6fcbeca98d
pkihiu <p.kihiu@alustudent.com>
2024-04-25 18:31:43 +0200

    update reser

---
+++ src/components/Reservation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ocean from '../assets/ocean.jpg';
+
+const Reservation = () => {
+    return (
+        <div className='flex'>
+            <img src={ocean} alt="Ocean" />
+        </div>
+    )
+}
+
+export default Reservation;
