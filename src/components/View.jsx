da75db00bb8921c116f44e2f1fd2f0b5ea3be199
pkihiu <p.kihiu@alustudent.com>
2024-06-17 10:57:30 +0300

    update View

---
+++ src/components/View.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import BoraBora from '../assets/borabora.jpg';
+import Maldives2 from '../assets/maldives2.jpg';
+import Maldives from '../assets/maldives.jpg';
+import Mombasa from '../assets/Mombas.jpg';
+import Dubai from '../assets/Dubai.jpg';
+
+const View = () => {
+  return (
+    <div className='max-w-[1240px] mx-auto py-16 px-4 text-center'>
+    <h1>All-Inclusive Resorts</h1>
+    <p className='py-4'>On the Caribbean's Best Beaches</p>
+    <div className='grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4'>
+        <img className='w-full h-full object-cover col-span-2 md:col-span-3 row-span-2' src={Maldives} alt="/" />
+        <img className='w-full h-full object-cover' src={BoraBora} alt="/" />
+        <img className='w-full h-full object-cover' src={Maldives2} alt="/" />
+        <img className='w-full h-full object-cover' src={Mombasa} alt="/" />
+        <img className='w-full h-full object-cover' src={Dubai} alt="/" />
+    </div>
+</div>
+  )
+}
+
+export default View
