f8a3b8cde65100368b66bb74406a335430d45e7e
pkihiu <p.kihiu@alustudent.com>
2024-06-17 11:40:22 +0300

    update view

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -8,9 +8,9 @@ import Dubai from '../assets/Dubai.jpg';
 
 const View = () => {
   return (
-    <div className='max-w-[1240px] mx-auto py-16 px-4 text-center'>
-    <h1>All-Inclusive Resorts</h1>
-    <p className='py-4'>On the Caribbean's Best Beaches</p>
+    <div className='max-w-[1240px] mx-auto py-5 px-4 text-center'>
+    <h1 className='text-3xl uppercase text-slate-900 text-bold'>All-Inclusive Resorts</h1>
+    <p className='py-2 text-xl text-gray-500 text-bold'>On the Caribbean's Best Beaches</p>
     <div className='grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4'>
         <img className='w-full h-full object-cover col-span-2 md:col-span-3 row-span-2' src={Maldives} alt="/" />
         <img className='w-full h-full object-cover' src={BoraBora} alt="/" />
